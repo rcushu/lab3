9d8d9da7b73691610d120f90bc62a9f5fc9c9299
ramisc <ramisc0801@gmail.com>
2023-10-25 15:03:10 -0700

    Update lab3

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,24 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  
+  @Test 
+	public void test2ReverseInPlace() {
+    int[] input1 = {4, 2, 3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3, 2, 4}, input1);
+	}
+
+  @Test 
+	public void test3ReverseInPlace() {
+    int[] input1 = {4, 2, 3, 1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{1, 3, 2, 4}, input1);
+	}
+
+  @Test
+  public void test2Reversed() {
+    int[] input1 = {4, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 4}, ArrayExamples.reversed(input1));
+  }
 }
